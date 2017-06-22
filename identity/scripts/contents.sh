afb456723f39c81c499d2b2c9317178a9da5ee27
greatyoulv <youlv@foxmail.com>
2017-06-22 18:07:34 +0800

    scripts

---
+++ identity/scripts/contents.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+touch /root/admin-openrc /root/demo-openrc
+echo "export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=pass
+export OS_AUTH_URL=http://controller:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2" > /root/admin-openrc
+
+echo "export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=pass
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2" > /root/demo-openrc
--- identity/verify/contents.sh
+++ identity/verify/contents.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 file1=/etc/keystone/keystone-paste.ini
-sed -i '/admin_token_auth/d' $file1 | grep -n '/^\[pipeline:public_api]' $file1
+sed -i 's/admin_token_auth/ /g' $file1 | grep -n '/^\[pipeline:public_api]' $file1
+unset OS_AUTH_URL OS_PASSWORD
+openstack --os-auth-url http://controller:35357/v3 \
+  --os-project-domain-name default --os-user-domain-name default \
+  --os-project-name admin --os-username admin token issue
+openstack --os-auth-url http://controller:5000/v3 \
+  --os-project-domain-name default --os-user-domain-name default \
+  --os-project-name demo --os-username demo token issue
+
