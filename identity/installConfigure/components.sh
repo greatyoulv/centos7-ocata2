343e52606034bc9ce665a0bdede9fd0b8ac75536
greatyoulv <youlv@foxmail.com>
2017-06-23 16:29:28 +0800

    reedit components.sh

--- identity/installConfigure/components.sh
+++ identity/installConfigure/components.sh
@@ -2,11 +2,11 @@
 file='/etc/keystone/keystone.conf'
 yum install openstack-keystone httpd mod_wsgi -y
 ##[database]
-sed -i '/\[database\]/{n;d}' $file |grep -n '^\[database\]' $file
-sed -i '/\[database\]/a connection = mysql+pymysql://keystone:pass@controller/keystone' $file |grep -n '^\[database\]' $file
+sed -i '/^\[database\]/{n;d}' $file |grep -n '^\[database\]' $file
+sed -i '/^\[database\]/a connection = mysql+pymysql://keystone:pass@controller/keystone' $file |grep -n '^\[database\]' $file
 ##[token]
-sed -i '/\[token\]/{n;d}' $file |grep -n '^\[token\]' $file
-sed -i '/\[token\]/a provider = fernet' $file |grep -n '^\[token\]' $file
+sed -i '/^\[token\]/{n;d}' $file |grep -n '^\[token\]' $file
+sed -i '/^\[token\]/a provider = fernet' $file |grep -n '^\[token\]' $file
 su -s /bin/sh -c "keystone-manage db_sync" keystone
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
