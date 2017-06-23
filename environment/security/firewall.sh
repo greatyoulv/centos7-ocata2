ddb6d9348e3e85f1ea626a7d07bfb422d98a4be3
greatyoulv <youlv@foxmail.com>
2017-06-23 16:33:53 +0800

    close selinux

--- environment/security/firewall.sh
+++ environment/security/firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-#setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
 systemctl stop firewalld.service
 systemctl disable firewalld.service
 
