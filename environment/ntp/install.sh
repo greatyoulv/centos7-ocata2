02383e1d4129751d4febc5ae188b0a38401ccf4c
greatyoulv <youlv@foxmail.com>
2017-06-23 12:08:14 +0800

    ntp reedit

--- environment/ntp/install.sh
+++ environment/ntp/install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-<<<<<<< HEAD
 yum install chrony -y
-=======
 yum install chrony
->>>>>>> 6339b45ddec29f3cab540ccc2df53dc41253e0c6
 systemctl enable chronyd.service
 systemctl start chronyd.service
 chronyc sources
