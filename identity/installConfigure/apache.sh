6586c4fd40e4fff7cc9a5c6ad0d3cc8087efd0be
greatyoulv <youlv@foxmail.com>
2017-06-27 15:24:36 +0800

    Update apache.sh

--- identity/installConfigure/apache.sh
+++ identity/installConfigure/apache.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 file1='/etc/httpd/conf/httpd.conf'
-sed -i '/ServerName/a ServerName controller' $file1 |grep -n '/^#ServerName' $file1
+sed -i '/^\#ServerName/{n;d}' $file1 |grep -n '^#ServerName' $file1
+sed -i '/^\#ServerName/a ServerName controller' $file1 |grep -n '^#ServerName' $file1
 ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
 systemctl enable httpd.service
 systemctl start httpd.service
