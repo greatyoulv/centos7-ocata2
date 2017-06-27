be9d3d1511bfbbe68f27d50c89cf6f66ef97a24b
greatyoulv <youlv@foxmail.com>
2017-06-27 15:40:46 +0800

    Update pre2.sh

--- identity/installConfigure/pre2.sh
+++ identity/installConfigure/pre2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mysql -uroot -e"GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
+mysql -uroot -ppass -e"GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
 IDENTIFIED BY 'pass';"
-mysql -uroot -e"GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
+mysql -uroot -ppass -e"GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
 IDENTIFIED BY 'pass';"
