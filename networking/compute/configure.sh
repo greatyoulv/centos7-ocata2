c7a2cdc0343c71e1b74e9c437a1b9011172a72bf
youlv <youlv@foxmail.com>
2017-07-01 16:58:37 +0800

    add networking/compute

---
+++ networking/compute/configure.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+systemctl restart openstack-nova-compute.service
+systemctl enable neutron-linuxbridge-agent.service
+systemctl start neutron-linuxbridge-agent.service
+
---
+++ networking/compute/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+yum install -y openstack-neutron-linuxbridge ebtables ipset
+
