03d63571a81b2af24e558faae397b47c66ff8e12
youlv <youlv@foxmail.com>
2017-07-02 10:47:57 +0800

    dashboard

--- dashboard/install.sh
+++ dashboard/install.sh
@@ -13,6 +13,28 @@ sed -i '/^\#ALLOWED_HOST/a ALLOWED_HOST = \['*'\]' $file1 |grep -n '^ALLOWED_HOS
 ##SESSION_ENGINE
 sed -i "N;/^CACHES/iSESSION_ENGINE = 'django.contrib.sessions.backends.cache'" $file1
 
+##CACHES
 sed -i "s/'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'/'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache'/" $file1
 
-sed -i "1,/'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache'/a 'LOCATION': 'controller:11211',
+#sed -i '145,155{/BACKEND/{n;d}}' $file1 |grep -n 'BACKEND' $file1
+sed -n '145,155{/BACKEND/p}' $file1 |sed -i "/BACKEND/a	'LOCATION': 'controller:11211'," $file1
+
+sed -i '180,190s/v2.0/v3/' $file1
+
+
+sed -i '/^\#OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT/{n;d}' $file1
+sed -i '/^\#OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT/a OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True' $file1 |grep -n '^OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT' $file1
+
+echo '\nOPENSTACK_API_VERSIONS = {
+    "identity": 3,
+    "image": 2,
+    "volume": 2,
+}' >> $file1
+
+sed -i '/^\#OPENSTACK_KEYSTONE_DEFAULT_DOMAIN/{n;d}' $file1
+sed -i '/^\#OPENSTACK_KEYSTONE_DEFAULT_DOMAIN/a OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = "Default"' $file1 |grep -n '^OPENSTACK_KEYSTONE_DEFAULT_DOMAIN' $file1
+
+sed -i '/^\#OPENSTACK_KEYSTONE_DEFAULT_ROLE/{n;d}' $file1
+sed -i '/^\#OPENSTACK_KEYSTONE_DEFAULT_ROLE/a OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"' $file1 |grep -n '^OPENSTACK_KEYSTONE_DEFAULT_ROLE' $file1
+
+systemctl restart httpd.service memcached.service
